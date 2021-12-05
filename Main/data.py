f028c39849d68bb1677b35bcb1946b2af7df0be0
AlexXu136 <anx019@ucsd.edu>
2021-12-04 21:44:20 -0800

    data.py complete, model code starting point

--- Main/data.py
+++ Main/data.py
@@ -1,1 +1,98 @@
+import csv
+import sys
+import torch
+import pickle
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+from transformers import BertTokenizer, BertModel
+from tqdm import tqdm
+from collections import defaultdict
 
+FILE_DIR = "../Data/train.csv"
+
+# Bert tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# Load pre-trained model
+model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states = False)
+# maximum length of tokens
+T = 50
+
+# only for demonstration
+def token_lengths(file_name: str):
+    data = defaultdict(dict)
+    with open(file_name, "r", encoding="utf8") as f:
+        reader = csv.reader(f, delimiter=",")
+        ids_save = []
+        token_lengths = []
+        count = 0
+        count_lessthan = 0
+        for i, line in enumerate(reader):
+            if i > 0:
+                marked_utterance = "[CLS] " + line[3]
+                tokenized_utterance = tokenizer.tokenize(marked_utterance)
+                token_lengths.append(len(tokenized_utterance))
+                count = count + 1
+                if len(tokenized_utterance) < 50:
+                    count_lessthan = count_lessthan + 1
+        print(count_lessthan/count)
+        plt.hist(token_lengths, density=False, bins=30) 
+        plt.ylabel('Probability')
+        plt.xlabel('Data')
+        plt.show()
+
+
+def process_sentences(file_name: str):
+    data = defaultdict(dict)
+    with open(file_name, "r", encoding="utf8") as f:
+        reader = csv.reader(f, delimiter=",")
+        ids_save = []
+        for i, line in enumerate(reader):
+            if i > 0:
+                data[line[0]]['keyword'] = line[1]
+                data[line[0]]['location'] = line[2]
+                data[line[0]]['text'] = line[3]
+                data[line[0]]['target'] = line[4]
+                data[line[0]]['embedding'] = Bert_embed(line[3])
+                ids_save.append(line[0])
+            if i % 20 == 0:
+                print(i, " / 10873")
+    return data, ids_save
+
+def Bert_embed(utterance: str):
+    # Tokenize the utterance
+    marked_utterance = "[CLS] " + utterance
+    tokenized_utterance = tokenizer.tokenize(marked_utterance)
+    # truncate or pad
+    if len(tokenized_utterance) >= T:
+        fixed_tokens = tokenized_utterance[:T]
+        attention_mask = [1] * T
+
+    else:
+        fixed_tokens = ["[PAD]" for i in range(T)]
+        fixed_tokens[:len(tokenized_utterance)] = tokenized_utterance
+        attention_mask = [1] * len(tokenized_utterance) + [0] * (T - len(tokenized_utterance))
+    indexed_tokens = tokenizer.convert_tokens_to_ids(fixed_tokens)
+    # Convert inputs to PyTorch tensors
+    tokens_tensor = torch.tensor([indexed_tokens])
+    attention_tensor = torch.tensor([attention_mask])
+    # get embedding
+    model.eval()
+    with torch.no_grad():
+        outputs = model(tokens_tensor, attention_mask = attention_tensor)
+    # output (all tokens of last layer) is of shape 768* 1
+    output = torch.transpose(outputs[0][0, :, :], 0, 1)
+    print(output.size())
+    return output
+
+if __name__ == '__main__':
+    # token_lengths(FILE_DIR)
+
+    data, ids = process_sentences(FILE_DIR)
+    # save
+    with open("dict.pkl", "wb") as tf:
+        pickle.dump(data,tf)
+    with open("ids.pkl", "wb") as tf:
+        pickle.dump(ids,tf)
+
+        
--- Main/model.py
+++ Main/model.py
@@ -1,1 +1,57 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from torch.utils.tensorboard import SummaryWriter
+from torchsummary import summary
+from torch.utils.data import DataLoader
+
+device = 'cuda' if (torch.cuda.is_available) else 'cpu'
+
+EMBEDDING_DIM = 768
+HIDDEN_DIM = 100
+
+#example
+#sentences = ["i like fresh bread", "i hate stale bread"]
+#split into set -> {i, like, hate, fresh, stale, bread}
+#index sentences dim -> (2,4) 
+#arbitrary embedding (2,4,10)
+#embed dim = 10
+#hidden dim = 64
+#lstm input shape (batch_size, seq_len, features) when batch_first=True
+#lstm output shape (batch_size, seq_len, hidden_size)
+#lstm(embed dim, hidden dim, )
+
+#Temp embedding, word2vec
+train_set = pd.read_csv("../Data/train.csv")
+train_set.shape
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        #BERT Embedding -> LSTM -> Linear
+
+        # width ~ 40
+        self.lstm_1 = nn.LSTM(EMBEDDING_DIM, HIDDEN_DIM) #(embedding dim, hidden dim)
+        self.dropout_1 = nn.Dropout(0.2, inplace=True)
+        self.lstm_2 = nn.LSTM()
+        self.linear_1 = nn.Linear(HIDDEN_DIM, 2)
+
+
+    def forward(self,x):
+        y = self.lstm_1(x)
+        y = self.linear_1(y)
+
+        return y
+
+model = Net()
+print(model)
+
+
 
